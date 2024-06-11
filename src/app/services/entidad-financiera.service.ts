cd6ab871b1317f190eca63902d9fad8c9e04f4ec
slipknot123zxc@gmail.com <134978454+ChinaskiHank@users.noreply.github.com>
2024-06-10 20:26:32 -0500

    Front Registro entidad financiera

--- src/app/services/entidad-financiera.service.ts
+++ src/app/services/entidad-financiera.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EntidadFinanciera } from '../models/entidad-financiera.model';
+import { AppSettings } from '../app.settings';
+import { Observable } from 'rxjs';
+
+const urlRegistro = AppSettings.API_ENDPOINT+ '/EntidadFinancieraRegistro'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntidadFinancieraService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  registro(entidad:EntidadFinanciera):Observable<any>{
+    return this.http.post(urlRegistro,entidad)
+  }
+
+  validacionNombre(nombre:String):Observable<any>{
+    console.log('>> Service >> validacionNombre[inicio]'+nombre)
+    return this.http.get<any>(urlRegistro+'/validaNombreEntidadFinanciera?nombre='+nombre);
+  }
+//
+  listar(idDataCatalogo?:any):Observable<EntidadFinanciera[]>{
+    return this.http.get<EntidadFinanciera[]>(urlRegistro+"/listar?idDataCatalogo=" + idDataCatalogo);
+  }
+
 }
+
