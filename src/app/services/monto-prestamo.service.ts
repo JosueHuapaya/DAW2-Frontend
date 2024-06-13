6f2385d37bc09cb23b415766a75235cc0a239da1
alejandra2811 <i202122798@cibertec.edu.pe>
2024-06-13 18:57:30 -0500

    monto Prestamp Alejanra Bolaños

--- src/app/services/monto-prestamo.service.ts
+++ src/app/services/monto-prestamo.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MontoPrestamo } from '../models/monto-prestamo.model';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../app.settings';
+
+
+const baseUrlPrueba = AppSettings.API_ENDPOINT+ '/monto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MontoPrestamoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  registrar(data:MontoPrestamo):Observable<any>{
+    return this.http.post(baseUrlPrueba, data);
+  }
 }
