9f9afba39f0426ed5e0aacb51ea9de6795f45c1f
JosueHuapaya <josuehuapaya@hotmail.com>
2024-06-13 22:30:33 -0500

    Registro Grupo terminado

--- src/app/models/grupo.model.ts
+++ src/app/models/grupo.model.ts
@@ -1,2 +1,16 @@
+import { Ubigeo } from "./ubigeo.model";
+import { Usuario } from "./usuario.model";
+
 export class Grupo {
+    idGrupo?: number;
+    descripcion?:string;
+    ubigeo?: Ubigeo;
+    departamento?:Ubigeo;
+    provincia?:Ubigeo;
+    distrito?:Ubigeo;
+    lider?:Usuario;
+    usuarioLider?:Usuario;
+    usuarioSuperior?:Usuario;
+    usuarioRegistro?: Usuario;
+    usuarioActualiza?: Usuario
 }
