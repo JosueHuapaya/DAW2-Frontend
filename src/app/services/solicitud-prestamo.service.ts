5696e51003a90211d2836aaa36037a6072086812
AlejandroHC11 <alejandrohinostr@gmail.com>
2024-06-29 06:18:23 -0500

    PC1-Hinostroza

--- src/app/services/solicitud-prestamo.service.ts
+++ src/app/services/solicitud-prestamo.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SolicitudPrestamo } from '../models/solicitud-prestamo.model';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../app.settings';
+
+const baseUrl = AppSettings.API_ENDPOINT+ '/solicitud';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolicitudPrestamoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  registrar(data:SolicitudPrestamo):Observable<any>{
+    return this.http.post(baseUrl, data);
+  }
+
+  listaPrestatariosTotales(): Observable<any> {
+    return this.http.get(baseUrl + '/prestatarios', {});
+  }
 }
